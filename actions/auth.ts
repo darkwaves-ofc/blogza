f03c6b4f787c67f325f37d97f34635b1421adca5
Dark Waves Tech <89140466+darkwaves-ofc@users.noreply.github.com>
2025-01-10 11:46:39 +0530

    `feat: updated .env.example and auth.ts to include new environment variables and handle admin login logic`

--- actions/auth.ts
+++ actions/auth.ts
@@ -30,7 +30,6 @@ export async function checkAdminAuth() {
   } catch (error) {
     return { isAuthenticated: false };
   }
-
 }
 
 export async function handleAdminLogin({
@@ -40,36 +39,41 @@ export async function handleAdminLogin({
   username: string;
   password: string;
 }) {
-  const adminUsername = process.env.ADMIN_USERNAME;
-  const adminPassword = process.env.ADMIN_PASSWORD;
-  const jwtSecret = process.env.JWT_SECRET_KEY;
-  if (!adminUsername || !adminPassword || !jwtSecret) {
-    return { success: false, error: "Environments are not setup!" };
+  try {
+    const adminUsername = process.env.ADMIN_USERNAME;
+    const adminPassword = process.env.ADMIN_PASSWORD;
+    const jwtSecret = process.env.JWT_SECRET_KEY;
+    if (!adminUsername || !adminPassword || !jwtSecret) {
+      return { success: false, error: "Environments are not setup!" };
+    }
+
+    if (!username || !password) {
+      return { success: false, error: "Username and password are required!" };
+    }
+
+    if (username === adminUsername && password === adminPassword) {
+      const token = await new SignJWT({
+        username,
+        role: "admin",
+      })
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime("24h")
+        .sign(new TextEncoder().encode(jwtSecret));
+
+      cookies().set("adminToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 60 * 60 * 24, // 24 hours
+      });
+
+      return { success: true };
+    }
+
+    return { success: false, error: "Invalid credentials" };
+  } catch (error) {
+    console.error("Error in handleAdminLogin:", error);
+    return { success: false, error: "An error occurred" };
   }
-
-  if (!username || !password) {
-    return { success: false, error: "Username and password are required!" };
-  }
-
-  if (username === adminUsername && password === adminPassword) {
-    const token = await new SignJWT({
-      username,
-      role: "admin",
-    })
-      .setProtectedHeader({ alg: "HS256" })
-      .setIssuedAt()
-      .setExpirationTime("24h")
-      .sign(new TextEncoder().encode(jwtSecret));
-
-    cookies().set("adminToken", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 60 * 60 * 24, // 24 hours
-    });
-
-    return { success: true };
-  }
-
-  return { success: false, error: "Invalid credentials" };
 }
